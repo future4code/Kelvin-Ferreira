ed3e8816fb37135bd8f1436c1b2092aac65cede8
KelvinSaFerreira <kelvin-sa@live.com>
2021-04-01 20:21:24 -0300

    Atividade sobre Operadores do Js

---
+++ quinzena3/operadores/index.js
@@ -0,0 +1,54 @@
+/*1)
+a) false;
+b) false;
+c) true;
+d) Boolean.*/
+
+/*2)
+a) undefined
+b) null
+c) 11
+d) 3
+e)  
+f) 9
+*/
+
+// 1)
+// let idade = Number(prompt("Qual é sua idade?"))
+// let idadeAmigo = Number(prompt("Qual a idade de seu melhor amigo?"))
+
+// console.log("Sua idade é maior que a de seu amigo?", idade > idadeAmigo)
+// console.log("Diferença de idade entre você e seu amigo:", idade - idadeAmigo, "anos")
+
+// 2)
+// let numeroPar = Number(prompt("Digite um número par"))
+// let divisao = numeroPar % 2
+// console.log(divisao)
+// todos os números pares quando divididos por 2 tem o resto de 0
+// todos os números ímpares quando divididos por 2 tem o resto de 1
+
+// 3)
+// let listaDeTarefas = []
+
+// let primeiraTarefa = prompt("Digite uma tarefa que você tem no dia")
+// listaDeTarefas.push(primeiraTarefa)
+
+// let segundaTarefa = prompt("Digite uma segunda tarefa que você tem no dia")
+// listaDeTarefas.push(segundaTarefa)
+
+// let terceiraTarefa = prompt("Digite uma terceira tarefa que você tem no dia")
+// listaDeTarefas.push(terceiraTarefa)
+
+// console.log(listaDeTarefas)
+
+// let tarefaCumprida = Number(prompt("Digite o índice da tarefa que você já realizou (0, 1, 2)"))
+// listaDeTarefas.splice(tarefaCumprida, 1)
+
+// console.log(listaDeTarefas)
+
+// 4)
+let userName = prompt("Digite seu nome completo")
+let userEmail = prompt("Digite seu endereço de e-mail")
+
+console.log("O e-mail "+userEmail+" foi cadastrado com sucesso. Seja bem-vinda(o), "+userName+"!")
+
