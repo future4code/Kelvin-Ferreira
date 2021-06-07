a2a6be4e6c821ac31fdc9b2c9dc95d5170e6390f
KelvinSaFerreira <kelvin-sa@live.com>
2021-06-06 23:14:15 -0300

    Adição Projeto Labefy v1.0

--- quinzena7/labefy/labefy/src/components/PlaylistPorNome.js
+++ quinzena7/labefy/labefy/src/components/PlaylistPorNome.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Playlist = styled.section`
+    width: 100%;
+`
+const Botao = styled.button`
+    background-color: #04c5c6;
+    border: none;
+    padding: 1vh 2vw;
+    border-radius: 8px;
+    color: white;
+    font-size: large;
+`
 export class PlaylistPorNome extends React.Component {
     render() {
         return (
-            <div>
-                <p>{this.props.nomePlaylist}</p>
-                <button onClick={this.props.deletarPlaylist}>Deletar</button>
-                <button onClick={this.props.pegarDetalhes}>Ver detalhes</button>
-            </div>
+            <Playlist>
+                <div>
+                    <p>{this.props.nomePlaylist}</p>
+                    <Botao onClick={this.props.deletarPlaylist}>Deletar</Botao>
+                    <Botao onClick={this.props.pegarDetalhes}>Abrir Playlist</Botao>
+                </div>
+                <hr />
+                <div>
+                    {this.props.addFaixa}
+                </div>
+            </Playlist>
         )
     }
 }
